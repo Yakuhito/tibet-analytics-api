355b61abe128f1dbe31eff55fe91b2891ce2ab74
yakuhito <y@kuhi.to>
2023-04-20 18:31:04 -0700

    database: use mysql

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
