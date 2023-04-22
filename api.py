41f52d96607ef3c2d4b70a7c0f8200cfd3ed96c6
yakuhito <y@kuhi.to>
2023-04-21 19:18:01 -0700

    /stats endpoint

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Depends
+from sqlalchemy import desc, func, BigInteger
 from sqlalchemy.orm import Session
-from sqlalchemy import desc
 from typing import List
 import models, database
 import os
@@ -36,6 +36,27 @@ async def get_transactions(pair_launcher_id: str, limit: int = 42, offset: int =
         .all()
     )
     return transactions
+
+@app.get("/stats")
+async def get_stats(db: Session = Depends(get_db)):
+    # Number of transactions
+    transaction_count = db.query(func.count(models.Transaction.coin_id)).scalar()
+
+    # Total value locked (two times the sum of all Pairs' xch_reserve)
+    total_value_locked = (
+        db.query(func.sum(models.Pair.xch_reserve)).scalar() or 0
+    ) * 2
+
+    # Total trade volume (sum of all Pairs' trade_volume, converted from string to int)
+    total_trade_volume = (
+        db.query(func.sum(func.cast(models.Pair.trade_volume, BigInteger))).scalar() or 0
+    )
+
+    return {
+        "transaction_count": transaction_count,
+        "total_value_locked": total_value_locked,
+        "total_trade_volume": total_trade_volume,
+    }
 
 @app.get("/")
 async def root():
