938320275a3ce3f1e2698082eb26d5189cb3b90a
yakuhito <y@kuhi.to>
2023-04-22 14:39:33 -0700

    new /transactions endpoint + cache

--- api.py
+++ api.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy import desc, func, BigInteger
+from cachetools import cached, TTLCache
 from sqlalchemy.orm import Session
 from typing import List
 import models, database
 import os
 
 app = APIRouter()
+cache = TTLCache(maxsize=100, ttl=10)
 
 # Dependency for getting DB session
 def get_db():
@@ -15,16 +17,19 @@ def get_db():
     finally:
         db.close()
 
+@cached(cache)
 @app.get("/router")
 async def get_router(db: Session = Depends(get_db)):
     router = db.query(models.Router).first()
     return router
 
+@cached(cache)
 @app.get("/pairs")
 async def get_pairs(db: Session = Depends(get_db)):
     pairs = db.query(models.Pair).all()
     return pairs
 
+@cached(cache)
 @app.get("/pair/{pair_launcher_id}")
 async def get_pair(pair_launcher_id: str, db: Session = Depends(get_db)):
     pair = db.query(models.Pair).filter(models.Pair.launcher_id == pair_launcher_id).first()
@@ -32,6 +37,7 @@ async def get_pair(pair_launcher_id: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Pair not found")
     return pair
 
+@cached(cache)
 @app.get("/transactions/{pair_launcher_id}")
 async def get_transactions(pair_launcher_id: str, limit: int = 42, offset: int = 0, db: Session = Depends(get_db)):
     transactions = (
@@ -44,6 +50,19 @@ async def get_transactions(pair_launcher_id: str, limit: int = 42, offset: int =
     )
     return transactions
 
+@cached(cache)
+@app.get("/transactions")
+async def get_transactions(limit: int = 42, offset: int = 0, db: Session = Depends(get_db)):
+    transactions = (
+        db.query(models.Transaction)
+        .order_by(desc(models.Transaction.height))
+        .limit(limit)
+        .offset(offset)
+        .all()
+    )
+    return transactions
+
+@cached(cache)
 @app.get("/stats")
 async def get_stats(db: Session = Depends(get_db)):
     # Number of transactions
