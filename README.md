b4f6b53022e780912c936a20124f5675972944aa
yakuhito <y@kuhi.to>
2023-05-07 16:00:22 -0700

    add more parameters to /transactions, remove /transactions/{pair_id}, and make /pairs use a better ranking for pairs (reserves)

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TibetSwap Analytics API
 
 ```
-ghcr.io/yakuhito/tibetswap-analytics-api:1.0.6
+ghcr.io/yakuhito/tibetswap-analytics-api:1.0.7
 ```
--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy import desc, func, BigInteger
 from cachetools import cached, TTLCache
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
 import models, database
 import os
 
@@ -23,14 +23,20 @@ async def get_router(db: Session = Depends(get_db)):
     router = db.query(models.Router).first()
     return router
 
+
 @cached(cache)
 @app.get("/pairs")
 async def get_pairs(db: Session = Depends(get_db)):
     return await _get_pairs(db)
 
 async def _get_pairs(db: Session):
-    pairs = db.query(models.Pair).all()
+    pairs = (
+        db.query(models.Pair)
+        .order_by(models.Pair.xch_reserve.desc())
+        .all()
+    )
     return pairs
+
 
 @cached(cache)
 @app.get("/pair/{pair_launcher_id}")
@@ -40,30 +46,44 @@ async def get_pair(pair_launcher_id: str, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Pair not found")
     return pair
 
-@cached(cache)
-@app.get("/transactions/{pair_launcher_id}")
-async def get_transactions(pair_launcher_id: str, limit: int = 42, offset: int = 0, db: Session = Depends(get_db)):
-    transactions = (
-        db.query(models.Transaction)
-        .filter(models.Transaction.pair_launcher_id == pair_launcher_id)
-        .order_by(desc(models.Transaction.height))
-        .limit(limit)
-        .offset(offset)
-        .all()
-    )
-    return transactions
 
 @cached(cache)
 @app.get("/transactions")
-async def get_transactions(limit: int = 42, offset: int = 0, db: Session = Depends(get_db)):
+async def get_transactions(
+    pair_launcher_id: Optional[str] = None,
+    operation: Optional[str] = None,
+    before_height: Optional[int] = None,
+    after_height: Optional[int] = None,
+    limit: int = 42,
+    offset: int = 0,
+    db: Session = Depends(get_db)
+):
+    if limit > 420:
+        raise HTTPException(status_code=400, detail="Limit cannot exceed 420")
+
+    query = db.query(models.Transaction)
+
+    if pair_launcher_id:
+        query = query.filter(models.Transaction.pair_launcher_id == pair_launcher_id)
+
+    if operation:
+        query = query.filter(models.Transaction.operation == operation)
+
+    if before_height:
+        query = query.filter(models.Transaction.height < before_height)
+
+    if after_height:
+        query = query.filter(models.Transaction.height > after_height)
+
     transactions = (
-        db.query(models.Transaction)
-        .order_by(desc(models.Transaction.height))
+        query.order_by(desc(models.Transaction.height))
         .limit(limit)
         .offset(offset)
         .all()
     )
+
     return transactions
+
 
 @cached(cache)
 @app.get("/stats")
@@ -86,6 +106,7 @@ async def get_stats(db: Session = Depends(get_db)):
         "total_value_locked": total_value_locked,
         "total_trade_volume": total_trade_volume,
     }
+
 
 @app.get("/")
 async def root():
