ed0ed412b7a8165dee71807e9b588e6f5250ebb8
Yak <y@kuhi.to>
2025-09-28 14:41:13 +0800

    that should do it

--- main.py
+++ main.py
@@ -38,31 +38,32 @@ async def router_and_pairs_sync_task():
 
     db: Session = database.SessionLocal()
     while True:
-        current_router = await api.get_router(db)
-        new_router, new_pairs = await sync.sync_router(current_router)
-        if new_router is not None:
-            db.commit()
-            db.refresh(new_router)
-        
-        for new_pair in new_pairs:
-            db.add(new_pair)
-            db.commit()
-
-        all_current_pairs = await api._get_pairs(db, wrap=False)
-        for current_pair in all_current_pairs:
-            new_pair, new_transactions, new_heights = await sync.sync_pair(current_pair)
-            if new_pair is not None:
+        for rcat in [False, True]:
+            current_router = await api.get_router(db, rcat=rcat)
+            new_router, new_pairs = await sync.sync_router(current_router)
+            if new_router is not None:
                 db.commit()
-                db.refresh(new_pair)
+                db.refresh(new_router)
             
-            for new_tx in new_transactions:
-                db.add(new_tx)
+            for new_pair in new_pairs:
+                db.add(new_pair)
                 db.commit()
 
-            for new_height in new_heights:
-                if not check_if_height_exists(new_height.height):
-                    db.add(new_height)
+            all_current_pairs = await api._get_pairs(db, wrap=False)
+            for current_pair in all_current_pairs:
+                new_pair, new_transactions, new_heights = await sync.sync_pair(current_pair)
+                if new_pair is not None:
                     db.commit()
+                    db.refresh(new_pair)
+                
+                for new_tx in new_transactions:
+                    db.add(new_tx)
+                    db.commit()
+
+                for new_height in new_heights:
+                    if not check_if_height_exists(new_height.height):
+                        db.add(new_height)
+                        db.commit()
 
         await asyncio.sleep(60)  # Wait for 1 minute
 
